41f5e60a0847095cb0e54efbf378025574cf9275
Roshan Chachire <103214789+Ronn26@users.noreply.github.com>
2024-01-06 19:59:43 +0000

    array

---
+++ .devcontainer/02_Basics/01_arrays.js
@@ -0,0 +1,35 @@
+// array
+
+const myArr = [0,1,2,3,4,5,6]
+const myHeros = ["Ben10","Junior-G","Shaktiman"]
+
+const myArr2 = new Array(1,2,3,4,5)
+// console.log(myHeros);
+
+//array methods
+
+// myArr2.push(6)
+// myArr2.push(7)
+// myArr2.pop()
+
+// myArr.unshift(8)
+// myArr.shift()
+
+// console.log(myArr.includes(8));
+// console.log(myArr.indexOf(4));
+
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+//slice,splice
+console.log("A",myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
