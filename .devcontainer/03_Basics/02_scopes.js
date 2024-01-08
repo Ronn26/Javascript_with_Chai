fd15aca137deb6aadb527bea48db7e75b602df8a
Roshan Chachire <103214789+Ronn26@users.noreply.github.com>
2024-01-08 17:03:53 +0000

    scopes

--- .devcontainer/03_Basics/02_scopes.js
+++ .devcontainer/03_Basics/02_scopes.js
@@ -8,3 +8,35 @@ if(true){
 console.log(a);
 // console.log(b)
 // console.log(c)
+
+function one(){
+    const username ="Roshan"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    two()
+}
+// one()
+if(true){
+    const username = "Roshan"
+    if(username === "Roshan"){
+        const website ="youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// +++++++++ intresting +++++++++++
+console.log(addone(5))
+
+function addone(num){
+    return num+1
+}
+
+
+const addTwo = function(num){
+    return num+2
+}
+addTwo(5)
